5813c51e0222b8f7530e2af9ac2094fd6c3c4e1f
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-04-02 15:31:48 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const form = document.querySelector('.search-form');
 const API_KEY = '34989150-F6282D23D45A19C7980767166'
 const input = document.querySelector('input[name="searchQuery"]')
-
 const body = document.body;
-console.log(body)
 
-let API_GALLERY = fetch("https://pixabay.com/api/?key=34989150-f6282d23d45a19c7980767166&q=car&image_type=photo&orientation=horizontal&safesearch=true").then(response => {
+
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+let API_GALLERY = fetch("https://pixabay.com/api/?key=34989150-f6282d23d45a19c7980767166&"+"q="+`${input.value}`+"&image_type=photo&orientation=horizontal&safesearch=true").then(response => {
     if (!response.ok) {
     throw new Error(response.status);
     }
@@ -16,5 +19,4 @@ let API_GALLERY = fetch("https://pixabay.com/api/?key=34989150-f6282d23d45a19c79
         console.log(item)
     })
 })
-
-
+})
