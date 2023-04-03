41a64b1e2ce16e0e5cf4ab13b4f967ed8b894700
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-04-03 21:04:58 +0200

    .

--- src/index.js
+++ src/index.js
@@ -4,6 +4,31 @@ const input = document.querySelector('input[name="searchQuery"]')
 const body = document.body;
 
 
+const renderHtml = (data) => {
+    const galleryContainer = document.createElement('div');
+    form.after(galleryContainer);
+
+    data.hits.map((item) => {
+        galleryContainer.innerHTML += `<div class="photo-card">
+  <img src="${item.previewURL}" alt="${item.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${item.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views${item.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments${item.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads${item.downloads}</b>
+    </p>
+  </div>
+</div>`;
+        
+})
+}
 
 
 form.addEventListener('submit', (e) => {
@@ -14,9 +39,11 @@ let API_GALLERY = fetch("https://pixabay.com/api/?key=34989150-f6282d23d45a19c79
     }
     return response.json()
 }).then(data => {
-    data.hits.map((item) => {
-        body.innerHTML += `<img src="${item.previewURL}">`
-        console.log(item)
-    })
+    
+    if (data.hits === []) {
+        console.log('o prosze')
+    }
+renderHtml(data);
+  
 })
 })
