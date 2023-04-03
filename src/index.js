b4c384cd078c7ba669074ff6ff0753f49d63b24d
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-04-03 21:39:15 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const form = document.querySelector('.search-form');
 const API_KEY = '34989150-F6282D23D45A19C7980767166'
 const input = document.querySelector('input[name="searchQuery"]')
@@ -26,24 +28,29 @@ const renderHtml = (data) => {
     </p>
   </div>
 </div>`;
+       
         
 })
 }
 
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit',e => {
     e.preventDefault();
 let API_GALLERY = fetch("https://pixabay.com/api/?key=34989150-f6282d23d45a19c7980767166&"+"q="+`${input.value}`+"&image_type=photo&orientation=horizontal&safesearch=true").then(response => {
     if (!response.ok) {
     throw new Error(response.status);
     }
     return response.json()
-}).then(data => {
-    
-    if (data.hits === []) {
-        console.log('o prosze')
+}).then(data => { 
+
+    renderHtml(data);
+
+    if (data.total === 0) {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    } else {
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
     }
-renderHtml(data);
-  
+    
+    
 })
 })
