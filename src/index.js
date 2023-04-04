8e64295c270e189e8cd27184cc120f9a1f87fde0
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-04-04 19:59:26 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import Notiflix from 'notiflix';
-
+import axios from 'axios';
 const form = document.querySelector('.search-form');
 const API_KEY = '34989150-F6282D23D45A19C7980767166'
 const input = document.querySelector('input[name="searchQuery"]')
 const body = document.body;
 
+const fetchData = async () => {
+    try {
+    const response = await axios.get("https://pixabay.com/api/?key=34989150-f6282d23d45a19c7980767166&"+"q="+`${input.value}`+"&image_type=photo&orientation=horizontal&safesearch=true");
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 const renderHtml = (data) => {
     const galleryContainer = document.createElement('div');
@@ -27,30 +35,30 @@ const renderHtml = (data) => {
       <b>Downloads${item.downloads}</b>
     </p>
   </div>
-</div>`;
-       
-        
+</div>`; 
 })
 }
 
 
 form.addEventListener('submit',e => {
-    e.preventDefault();
-let API_GALLERY = fetch("https://pixabay.com/api/?key=34989150-f6282d23d45a19c7980767166&"+"q="+`${input.value}`+"&image_type=photo&orientation=horizontal&safesearch=true").then(response => {
-    if (!response.ok) {
-    throw new Error(response.status);
-    }
-    return response.json()
-}).then(data => { 
+  e.preventDefault();
+  
+fetchData()
+// let API_GALLERY = fetch("https://pixabay.com/api/?key=34989150-f6282d23d45a19c7980767166&"+"q="+`${input.value}`+"&image_type=photo&orientation=horizontal&safesearch=true").then(response => {
+//     if (!response.ok) {
+//     throw new Error(response.status);
+//     }
+//     return response.json()
+// }).then(data => { 
 
-    renderHtml(data);
+//     renderHtml(data);
 
-    if (data.total === 0) {
-        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-    } else {
-        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-    }
+//     if (data.total === 0) {
+//         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+//     } else {
+//         Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+//     }
     
     
-})
+// })
 })
