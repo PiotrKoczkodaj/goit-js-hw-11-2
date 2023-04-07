d250daa5dd5d96f99af9c5db05db6969099fd55b
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-04-07 23:41:47 +0200

    .

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 const form = document.querySelector('.search-form');
 const API_KEY = '34989150-F6282D23D45A19C7980767166'
 const input = document.querySelector('input[name="searchQuery"]')
-const body = document.body
+const btn = document.querySelector('button[type="submit"]');
 
 const fetchData = async () => {
     try {
       const response = await axios.get("https://pixabay.com/api/?key=34989150-f6282d23d45a19c7980767166&" + "q=" + `${input.value}` + "&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1");
-      console.log(response.data)
+      
       return response.data
       
   } catch (error) {
@@ -41,17 +41,35 @@ const data = await fetchData()
   </div>
 </div>`});
   
+    //  input.addEventListener('input', () => {
+    //   console.log('czesc');
+    //   galleryContainer.innerHTML = ''
+    //  })
+  
      if (data.total===0) {
         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
     } else {
         Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
   }
  
+  input.addEventListener('keydown', () => {
+    galleryContainer.innerHTML = ''
+  })
+  
 }
 
-form.addEventListener('submit',e => {
+form.addEventListener('submit', e => {
+  
   e.preventDefault();
   renderHtml()
+
+  const value = input.value;
+  localStorage.setItem("inputValue", value);
+  let storage = localStorage.getItem('inputValue');
+
+
+ 
 })
+
 
 
