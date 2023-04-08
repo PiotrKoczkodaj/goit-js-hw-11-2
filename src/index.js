bd7b80bc1726f19b8246a12a4babdedfb686e93a
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-04-08 10:03:41 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
 const form = document.querySelector('.search-form');
-const API_KEY = '34989150-F6282D23D45A19C7980767166'
-const input = document.querySelector('input[name="searchQuery"]')
+const API_KEY = '34989150-F6282D23D45A19C7980767166';
+const input = document.querySelector('input[name="searchQuery"]');
 const btn = document.querySelector('button[type="submit"]');
-const body = document.body
+const body = document.body;
+let pageValue = 1;
 const fetchData = async () => {
     try {
-      const response = await axios.get("https://pixabay.com/api/?key=34989150-f6282d23d45a19c7980767166&" + "q=" + `${input.value}` + "&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1");
-
-      return response.data
-
+      const response = await axios.get("https://pixabay.com/api/?key=34989150-f6282d23d45a19c7980767166&" + "q=" + `${input.value}` + "&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&"+`page=${pageValue}`);
+      return response
   } catch (error) {
     console.error(error);
   }
 }
 
-const renderHtml = async () => {
+const renderHtml = async (data) => {
 const galleryContainer = document.createElement('div');
   galleryContainer.classList.add('gallery-container');
   form.after(galleryContainer);
 
-const data = await fetchData()
-  data.hits.map((item) => {
+ data = await fetchData()
+  data.data.hits.map((item) => {
     galleryContainer.innerHTML += `<div class="photo-card">
   <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" height=220 width=340/>
   <div class="info">
@@ -41,9 +40,16 @@ const data = await fetchData()
   </div>
 </div>`});
 
-
-
-
+  const paginationBtn = document.createElement('button');
+  paginationBtn.textContent = 'Load more';
+  galleryContainer.append(paginationBtn);
+//////////////////////////////////////////////////////////////////////////////
+  paginationBtn.addEventListener('click', () => {
+    pageValue += 1;
+    
+    
+  })
+  ////////////////////////////////////////////////
      if (data.total===0) {
         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
     } else {
@@ -52,14 +58,12 @@ const data = await fetchData()
 
 }
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit',(e) => {
 
-  e.preventDefault();
+  e.preventDefault()
+  fetchData()
   renderHtml()
-
 })
 
-input.addEventListener('keydown', () => {
-  body.childNodes[2].innerHTML = '';  
-})
+
 
